bb0249730ee9c2bcc9a66655f9181717152abd12
Benjamin Campbell <benji@benjica.com>
2014-10-02 12:31:58 -0600

    Add Sum and Divisor Algorithms

--- src/12/problem_12.go
+++ src/12/problem_12.go
@@ -5,5 +5,29 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hello world!\n")
+
+	for i := 1; i <= 10; i++ {
+		sum := SumN(i)
+		list := ListDivisors(sum)
+
+		fmt.Printf("%02d: %d %v\n", i, sum, list)
+	}
+}
+
+func SumN(n int) int {
+	return (n * (n + 1)) / 2
+}
+
+func ListDivisors(n int) []int {
+
+	divisors := make([]int, 0)
+
+	for i := 1; i <= n/2; i++ {
+		if n%i == 0 {
+			divisors = append(divisors, i)
+		}
+	}
+
+	divisors = append(divisors, n)
+	return divisors
 }
