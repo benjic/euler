db3f9a00baea6c2446955ca0857aabd13f31c3c6
Benjamin Campbell <benji@benjica.com>
2014-10-02 19:51:46 -0600

    Implemented Solution
    Changed types to make max easier

--- src/18/problem_18.go
+++ src/18/problem_18.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -12,18 +13,26 @@ func main() {
 	file, _ := os.Open(os.Args[1])
 
 	scanner := bufio.NewScanner(file)
-	triangle := make([][]int64, 0)
+	triangle := make([][]float64, 0)
 
 	for scanner.Scan() {
 
-		line := make([]int64, 0)
+		line := make([]float64, 0)
 
 		for _, elem := range strings.Split(scanner.Text(), " ") {
-			num, _ := strconv.ParseInt(elem, 10, 64)
+			num, _ := strconv.ParseFloat(elem, 64)
 			line = append(line, num)
 		}
 
 		triangle = append(triangle, line)
+	}
+
+	for row := len(triangle) - 2; row >= 0; row-- {
+		for sub := 0; sub < len(triangle[row]); sub++ {
+			triangle[row][sub] = math.Max(
+				triangle[row][sub]+triangle[row+1][sub],
+				triangle[row][sub]+triangle[row+1][sub+1])
+		}
 	}
 
 	fmt.Printf("%v", triangle)
