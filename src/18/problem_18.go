1cba965f338636da0415915752f8db77526c2998
Benjamin Campbell <benji@benjica.com>
2014-10-02 19:37:00 -0600

    Read Data

--- src/18/problem_18.go
+++ src/18/problem_18.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	file, _ := os.Open(os.Args[1])
+
+	scanner := bufio.NewScanner(file)
+	triangle := make([][]int64, 0)
+
+	for scanner.Scan() {
+
+		line := make([]int64, 0)
+
+		for _, elem := range strings.Split(scanner.Text(), " ") {
+			num, _ := strconv.ParseInt(elem, 10, 64)
+			line = append(line, num)
+		}
+
+		triangle = append(triangle, line)
+	}
+
+	fmt.Printf("%v", triangle)
 }
