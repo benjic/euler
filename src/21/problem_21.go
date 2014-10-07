9b3847e1ea8cfc49eded6f067601cf7375606a5d
Benjamin Campbell <benjamin.campbell@mso.umt.edu>
2014-10-07 13:58:55 -0600

    Initial Problem solution

--- src/21/problem_21.go
+++ src/21/problem_21.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"./numbers"
+)
 
 func main() {
 
-	fmt.Println("Hello world!")
+	for i := int64(220); i < 225; i++ {
+
+		a := numbers.Divisors(i)
+		a_sum := sum(a[:len(a)-1])
+		b := numbers.Divisors(a_sum)
+		b_sum := sum(b[:len(b)-1])
+
+		fmt.Printf("i: %d a: %d b: %d\n", i, a_sum, b_sum)
+	}
+}
+
+func sum(list []int64) int64 {
+
+	sum := int64(0)
+
+	for _, v := range list {
+		sum += v
+	}
+
+	return sum
 }
